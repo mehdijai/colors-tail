caa038f673296a766d349ea5e6bb27a13e33888b
= <=>
2022-03-24 23:37:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Colors tail
+
+A simple HTML, CSS and JavaScript project with the basics of DOM manipulation.
+When you click the correspond color, the text block aside change according to the input values.
---
+++ script.js
@@ -0,0 +1,13 @@
+const p = document.querySelector("p");
+
+function setBackgroundColor(color){
+    p.style.backgroundColor = color
+}
+
+function setBorderColor(color){
+    p.style.border = "3px solid " + color
+}
+
+function setTextColor(color){
+    p.style.color = color
+}
